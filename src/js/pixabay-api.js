3a5d82da32edae83c4c5fa1eeec2270a7827633f
LesyaKot <lesyakot83@gmail.com>
2024-02-20 17:07:48 +0500

    add header

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,7 @@
 
 const KEY = '42468615-7a21ca39eb8f796f5c09d98b3';
 const BASE_URL = 'https://pixabay.com/api/';
-const QUERY = 'dogs';
+const QUERY = '';
 const IMAGE_TYPE = 'photo';
 const ORIENTATION = 'horizontal';
 const SAFESEARCH = true;
@@ -25,7 +25,7 @@ function getImages(){
         })
 }
 getImages().then(data => {
-    const images = data.hits.slice(0, 10);
+    const images = data.hits.slice(0, 9);
     
     document.body.innerHTML = images.map(({webformatURL}) => {
         return `<img src="${webformatURL}" width="360" height="300">`
