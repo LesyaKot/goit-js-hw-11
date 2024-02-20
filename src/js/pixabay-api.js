f9f4ed392e9668718bb32da3852e7ca9b25a27f4
LesyaKot <lesyakot83@gmail.com>
2024-02-20 22:13:09 +0500

    add js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,9 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-
-
+const input = document.querySelector('#data-search');
+const button = document.querySelector('[data-start]');
+const galleryElement = document.querySelector('.gallery');
 
 const KEY = '42468615-7a21ca39eb8f796f5c09d98b3';
 const BASE_URL = 'https://pixabay.com/api/';
@@ -9,27 +12,41 @@ const IMAGE_TYPE = 'photo';
 const ORIENTATION = 'horizontal';
 const SAFESEARCH = true;
 
-const LINK = `${BASE_URL}?key=${KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${true}`;
+const LINK = `${BASE_URL}?key=${KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH}`;
 
+galleryElement.innerHTML = '';
 
-function getImages(){
-    return fetch(LINK)
-        .then(response =>{
-            if(!response.ok){
-                throw new Error('Image error!')
-            }
-            return response.json()
-        })
-        .catch(error =>{
-            alert('ERROR')
-        })
-}
-getImages().then(data => {
+fetch(LINK)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Image error!');
+    }
+    return response.json();
+  })
+  .then(data => {
     const images = data.hits.slice(0, 9);
-    
-    document.body.innerHTML = images.map(({webformatURL}) => {
-        return `<img src="${webformatURL}" width="360" height="300">`
-    }).join('')
+
+    galleryElement.innerHTML = images
+      .map(({ webformatURL }) => {
+        return `<img src="${webformatURL}" width="360" height="300">`;
+      })
+      .join('');
+  })
+  .catch(error => {
+    iziToast.error({
+      title: 'Error',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+    });
+  });
+
+input.addEventListener('input', event => {
+  event.target.value.trim();
+});
+
+button.addEventListener('click', event => {
+  event.preventDefault();
+  
+});
 
 
-})
